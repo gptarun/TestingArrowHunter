8c9b7f094c59751b27e67f0178b6b1576ff4088a
tarung1201 <tarung1201@gmail.com>
2018-11-13 21:48:47 +0530

    Added gate and add angle for ballista..
    Karna pada.. cz commit kar he rhaa tha me :P

--- TestingArrowHunt/Assets/Scripts/BallistaRotationScript.cs
+++ TestingArrowHunt/Assets/Scripts/BallistaRotationScript.cs
@@ -5,40 +5,45 @@ using UnityEngine;
 public class BallistaRotationScript : MonoBehaviour {
     public Animator animator;
     bool isTouch = false;
-    Vector3 startPosition;
-    Vector3 endPosition;
+    public Camera myCam;
+    private Vector3 screenPos;
+    private float angleOffset;
     float angle = 90;
+    int calculatedAngle;
     // Use this for initialization
     void Start () {
-		
-	}
+        myCam = Camera.main;
+        animator = GetComponent<Animator>();
+        Debug.Log("initialization");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.touchCount > 0)
+        //This fires only on the frame the button is clicked
+        if (Input.GetMouseButtonDown(0))
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
-            {
-                isTouch = true;
-                animator.SetBool("isTouch", isTouch);
-                startPosition = touch.position;                
-            }
-            else if (touch.phase == TouchPhase.Moved)
-            {
-                isTouch = true;
-                endPosition = touch.position;
-                Debug.Log("Difference - " + (endPosition.y - startPosition.y));
-                animator.SetBool("isTouch", isTouch);
-                //Add a logic to create an angle
-                angle = endPosition.y - startPosition.y;
-                animator.SetFloat("angle", angle);                              
-            }
-            else if (touch.phase == TouchPhase.Ended)
-            {
-                isTouch = false;
-                animator.SetBool("isTouch", isTouch);
-            }
+            isTouch = true;
+            Debug.Log("Pressed mouse down button");
+            screenPos = myCam.WorldToScreenPoint(transform.position);
+            Vector3 v3 = Input.mousePosition - screenPos;
+            
+            angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(v3.y, v3.x)) * Mathf.Rad2Deg;
         }
-	}
+        //This fires while the button is pressed down
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 v3 = Input.mousePosition - screenPos;
+            float angle = Mathf.Atan2(v3.y, v3.x) * Mathf.Rad2Deg;
+            calculatedAngle = (int)(angle + angleOffset);
+            Debug.Log("angle + angleOffset : " + calculatedAngle);
+            animator.SetBool("isTouch", isTouch);
+            animator.SetInteger("Angle", calculatedAngle);
+        }
+        if (Input.GetMouseButtonUp(0) && isTouch==true)
+        {
+            Debug.Log("Pressed mouse up button");
+            isTouch = false;
+            animator.SetBool("isTouch", isTouch);
+        }
+    }
 }
